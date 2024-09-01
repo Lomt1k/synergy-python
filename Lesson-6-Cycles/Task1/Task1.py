9cd8966d4fd227fc5bff2b45ad82ab2647b45db0
Vladimir Yashin <vladimir.yashin@mytona.com>
2024-09-01 17:44:12 +0300

    Урок №6. Циклы while и for

---
+++ Lesson-6-Cycles/Task1/Task1.py
@@ -0,0 +1,12 @@
+﻿# Задание N1
+
+# Сначала вводится число N, затем вводится ровно N целых чисел. Подсчитайте, сколько из них равны нулю, и выведите это количество.
+
+numbersCount = int(input('Введите число N: '))
+zeroCount = 0
+for i in range(0, numbersCount):
+    value = int(input('Введите число: '))
+    if value == 0:
+        zeroCount += 1
+
+print(f'Нуль был введён {zeroCount} раз')
