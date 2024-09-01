53e4afdda53b1c125cbe7b210590ebbe02483b5e
Vladimir Yashin <vladimir.yashin@mytona.com>
2024-09-01 18:03:11 +0300

    Небольшой фикс

--- Lesson-6-Cycles/Task3/Task3.py
+++ Lesson-6-Cycles/Task3/Task3.py
@@ -10,5 +10,5 @@ else:
     print('Все чётные числа на заданном отрезке:')
     for i in range(a, b + 1):
         if i % 2 == 0:
-            print(f'{i} ', end='')
+            print(i, end=' ')
     print()
