950f0c6976e82870213766869028ef088221c923
Vladimir Yashin <vladimir.yashin@mytona.com>
2024-09-03 09:56:11 +0300

    Урок №7. Строки

---
+++ Lesson-7-Strings/Task2/Task2.py
@@ -0,0 +1,15 @@
+﻿# Задание N2
+
+# Дана строка, длина которой не превосходит 1000. Вам требуется преобразовать все идущие подряд пробелы в один. Выведите измененную строку.
+
+text = input('Введите строку, из которой вы хотите убрать лишние пробелы (более одного пробела подряд): ')
+canUseSpace = True
+for char in text:
+    if char == ' ':
+        if canUseSpace:
+            print(char, end='')
+            canUseSpace = False
+    else:
+        canUseSpace = True
+        print(char, end='')
+print()
